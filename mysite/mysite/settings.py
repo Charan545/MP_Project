0461f4ab0e5d2d4abfcd4df0d1e09e25ce47551f
Charan545 <allagadacharan12@gmail.com>
2025-03-03 11:30:30 +0530

    Seventh Commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -82,12 +82,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import os
+import dj_database_url  # Ensure this is at the top
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
+
+
 
 
 # Password validation
